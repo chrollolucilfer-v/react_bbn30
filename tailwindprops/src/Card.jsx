fcd2c68282522ee1e3603fc65c4555c5e9cd884b
chrollolucilfer-v <guptabhaskar9897@gmail.com>
2026-06-27 13:21:21 +0530

    d

---
+++ tailwindprops/src/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = ({ channel }) => {
+  console.log(channel);
+  return <div>Card</div>;
+};
+
+export default Card;
+
+// props is currectly empty
+// questions for interview
+// Not a core js
+// counter for increment and decrement
+//
+onclick={addCounter}
+
+
+
+// what to do for that ??
